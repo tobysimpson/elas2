1acb8cc1126ff6de65bd832f08c50309d87ce846
Toby Simpson <toby.simpson@usi.ch>
2024-02-01 10:20:55 +0100

    Initial Commit

---
+++ elas2/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  elas2
+//
+//  Created by Toby Simpson on 01.02.24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
